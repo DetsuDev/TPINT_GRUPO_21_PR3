f95104cec6586f22f5c54df522f4ac894b9f0ddf
Sebastian <sebastianfuentesjhi@gmail.com>
2026-06-19 23:06:28 -0300

    cambios

---
+++ Entidades/Paciente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Paciente : Persona
+    {
+        public Paciente() { }
+        public int IdPaciente { set; get; }
+        public bool Estado { set; get; }
+
+        ///  El IdPersona ya se hereda de la clase Persona
+
+    }
+}

2aba4eff6c776927eea46c416f84df74fd269e05
Sebastian <sebastianfuentesjhi@gmail.com>
2026-06-19 21:24:31 -0300

    adapter para AcessoDatos y algunos error catchers

--- Datos/AccesoDatos.cs
+++ Datos/AccesoDatos.cs
@@ -13,16 +13,38 @@ namespace Datos
         
         private const string cadenaConexion = "Data Source = localhost\\SQLEXPRESS;Initial Catalog = ClinicaDB; Integrated Security = True";
 
+        private SqlDataAdapter ObtenerAdaptador(String consultaSql, SqlConnection cn)
+        {
+            SqlDataAdapter adaptador;
+            try
+            {
+                adaptador = new SqlDataAdapter(consultaSql, cn);
+                return adaptador;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
 
         public DataTable obtenerTabla(string consulta)
         {
             SqlConnection sqlConnection = new SqlConnection(cadenaConexion);
             sqlConnection.Open();
-            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(consulta, sqlConnection);
-            DataTable dataTable = new DataTable();
-            sqlDataAdapter.Fill(dataTable);
-            sqlConnection.Close();
-            return dataTable;
+            SqlDataAdapter sqlDataAdapter = ObtenerAdaptador(consulta, sqlConnection);
+
+            if (sqlDataAdapter != null)
+            {
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+                sqlConnection.Close();
+                return dataTable;
+            }
+            else
+            {
+                sqlConnection.Close();
+                return null;
+            }  
         }
 
         public void ejecutarConsulta(string consulta)
